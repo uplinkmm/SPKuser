d236a5b17dc3b1fc5573ced1655e3a96d2d5067b
kaungsattkhant <ookaung42@gmail.com>
2025-01-17 10:52:16 +0630

    done

--- app/Http/Action/SMSPoh.php
+++ app/Http/Action/SMSPoh.php
@@ -24,6 +24,7 @@ class SMSPoh
         $apiEndPoint = env('SMS_POH_API_END_POINT');
 
         $combined = "$apiKey:$apiSecret"; // Combine API key and secret in the format <apiKey>:<apiSecret>
+
         $apiToken = base64_encode($combined);
         //payload
         $payload = [
@@ -32,24 +33,7 @@ class SMSPoh
             "message" => "Your otp code is " . $this->customer->otp, // OTP message
         ];
 
-        // if ($decoded === $combined) {
-        //     ResponseMessage("The token is correctly generated and matches the format: {$decoded}",200);
-        // } else {
-        //     ResponseMessage("There is an issue with the token generation. Expected: {$combined}, Got: {$decoded}",200);
-        // }
-        // $apiToken = base64_encode("$apiKey>:<$apiSecret");
-        // $apiToken = base64_encode($apiKey.":".$apiSecret);
         try {
-            // Send POST request using HTTP client
-                // $response = Http::withHeaders([
-                //     'Authorization' => "Bearer {$apiToken}",
-                //     'Content-Type' => 'application/json',
-                // ])->post($apiEndPoint, [
-                //     'to' => $phone_number,
-                //     'message' => $message,
-                //     'from' => $from,
-                // ]);
-
             $response = Http::withHeaders([
                 'Authorization' => "Bearer {$apiToken}",
                 'Content-Type' => 'application/json',
@@ -57,12 +41,14 @@ class SMSPoh
     
             // Check for successful response
             if ($response->successful()) {
-                return response()->json(['success' => true, 'message' => 'OTP sent successfully.']);
+                // return response()->json(['success' => true, 'message' => 'OTP sent successfully.']);
+                return true;
             }
     
             // Log and return error response if request fails
             Log::error('Failed to send OTP:', ['response' => $response->body()]);
-            return response()->json(['success' => false, 'message' => 'Failed to send OTP.'], $response->status());
+            return false;
+            // return response()->json(['success' => false, 'message' => 'Failed to send OTP.'], $response->status());
         } catch (\Exception $e) {
             Log::error('Exception occurred while sending OTP:', ['error' => $e->getMessage()]);
             return response()->json(['success' => false, 'message' => 'An error occurred while sending OTP.'], 500);
