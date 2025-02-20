80101be9af63e1b2a960e10afb0ec412fbc9bf5b
kaungsattkhant <ookaung42@gmail.com>
2025-02-20 13:22:49 +0630

    done

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -17,8 +17,8 @@ class RegisterRequest extends APIRequest
                     return $query->where('is_verified', 1);
                 }),
             ],
-            'password' => 'required|min:6',
-            // 'password' => 'required|confirmed|min:6',
+            // 'password' => 'required|min:6',
+            'password' => 'required|confirmed|min:6',
             'otp' => 'required',
 
         ];
