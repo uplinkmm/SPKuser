f477f523ae9889de64bda52b1181c1856799038d
kaungsattkhant <ookaung42@gmail.com>
2025-02-20 13:16:57 +0630

    merge:nay/first commit

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Validation\Rule;
+use App\Http\Requests\APIRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class RegisterRequest extends APIRequest
+{
+    public function rules()
+    {
+        return [
+            'phone_number' => [
+                'required',
+                Rule::unique('customers', 'phone_number')->where(function ($query) {
+                    return $query->where('is_verified', 1);
+                }),
+            ],
+            'password' => 'required|min:6',
+            // 'password' => 'required|confirmed|min:6',
+            'otp' => 'required',
+
+        ];
+    }
+    public function authorize()
+    {
+        return parent::authorize();
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        parent::failedValidation($validator);
+    }
+}
