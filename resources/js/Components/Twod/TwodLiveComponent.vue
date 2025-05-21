59aae27fc07b9a53bc8c170af88e295e0f195386
NayPaingSoe <naypaingsoe042@gmail.com>
2025-05-21 19:53:12 +0630

    fix: live api

--- resources/js/Components/Twod/TwodLiveComponent.vue
+++ resources/js/Components/Twod/TwodLiveComponent.vue
@@ -2,22 +2,22 @@
     <div class="frame-container bg-white min-h-[100vh]">
         <Navbar title="2D" :need-auth="false" :back-btn="backBtn"></Navbar>
 
-        <div class="bg-[#FDC652] flex justify-center mt-2 rounded-lg gap-2 items-center">
-            <a
-                href="/history?game_id=1"
-                class=" text-center p-8"
-            >
+        <div
+            class="bg-[#FDC652] flex justify-center mt-2 rounded-lg gap-2 items-center"
+        >
+            <a href="/history?game_id=1" class="text-center p-8">
                 <i class="fal fa-file text-3xl pb-1 text-white"></i>
                 <p class="text-black">မှတ်တမ်း</p>
             </a>
-            <a
-                href="/2D_betting"
-                class=" text-center p-8"
-            >
-                <img src="../../../../public/img/billiard_2377303.png" class="w-10" alt="">
+            <a href="/2D_betting" class="text-center p-8">
+                <img
+                    src="../../../../public/img/billiard_2377303.png"
+                    class="w-10"
+                    alt=""
+                />
                 <p class="text-black">ထိုးမည်</p>
             </a>
-            <div class=" text-center p-8">
+            <div class="text-center p-8">
                 <a href="/winner_lists/1">
                     <i class="fal fa-users text-3xl pb-1 text-white"></i>
                     <p class="text-black">ထီပေါက်သူ</p>
@@ -150,6 +150,7 @@ export default {
             twoDList: [],
             // two_d:null,
             intervalId: null,
+            showSpinner: false,
         };
     },
 
@@ -165,19 +166,23 @@ export default {
             window.history.back();
         },
         async get2DList() {
+            if(this.showSpinner == false){
+            this.showSpinner = true;
             const response = await getApiData({
                 url: "https://admin.shwepaukkan.com/api/2d/live",
             });
             if (response.data) {
                 this.twoDList = response.data;
+                this.showSpinner = false;
             } else {
-                // console.log("some errors occur");
+                this.showSpinner = false;
+            }
             }
         },
     },
 
     created() {
-        this.intervalId = setInterval(this.get2DList, 3000);
+        this.intervalId = setInterval(() => this.get2DList(), 3000);
     },
 
     mounted() {
