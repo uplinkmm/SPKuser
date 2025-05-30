3b3c191cbfb910825c75d60b391ea9adf22bbf76
NayPaingSoe <naypaingsoe042@gmail.com>
2025-05-30 15:28:00 +0630

    fix: icon

--- resources/js/Components/Twod/TwodLiveComponent.vue
+++ resources/js/Components/Twod/TwodLiveComponent.vue
@@ -153,7 +153,6 @@ export default {
             showSpinner: false,
         };
     },
-
     methods: {
         // async get2DLive() {
         //     const response = await getApiData({ url: '/api/2d/live' });
