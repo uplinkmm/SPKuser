1fe4d81e504b9bfe32bdd49a584488916512dcc4
kaungsattkhant <ookaung42@gmail.com>
2025-07-08 11:25:52 +0630

    pushed marquee ads

--- app/Repositories/Ads/AdsRepository.php
+++ app/Repositories/Ads/AdsRepository.php
@@ -14,5 +14,11 @@ class AdsRepository implements AdsInterface
         ->where('type','ads')
         ->take(3)->get();
     }
+
+    public function getMarqueeAds($request){
+        return Ads::orderBy("id", "desc")
+        ->where('type','marquee')
+        ->get();
+    }
     
 }
