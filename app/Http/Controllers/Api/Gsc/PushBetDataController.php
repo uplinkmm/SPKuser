cbd2ca5e145a72ad1d791729d0dee260ab465e0d
kaungsattkhant <ookaung42@gmail.com>
2025-08-26 11:12:01 +0630

    done

--- app/Http/Controllers/Api/Gsc/PushBetDataController.php
+++ app/Http/Controllers/Api/Gsc/PushBetDataController.php
@@ -2,18 +2,21 @@
 
 namespace App\Http\Controllers\Api\Gsc;
 
+use App\Enums\CurrencyRate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
+use App\Enums\SlotWebhookResponseCode;
+use App\Services\Slot\SlotWebhookService;
 use App\Http\Requests\Gsc\GscWebhookRequest;
 use App\Http\Controllers\Api\V1\Webhook\Traits\GscWebhook;
 
 class PushBetDataController extends Controller
 {
   //
-    use GscWebhook;
+  use GscWebhook;
   public function pushBetData(GscWebhookRequest $batchRequest)
   {
     foreach ($batchRequest['wagers'] as $batch) {
@@ -23,27 +26,50 @@ class PushBetDataController extends Controller
       $request['request_time'] = $batchRequest->request_time;
       $request['url'] = $batchRequest->url();
       $userId = $request->getMember() ? $request->getMember()->id : null;
+      $currencyRate = CurrencyRate::fromName($batchRequest->currency);
+      if (!$currencyRate) {
+        return SlotWebhookService::buildGscResponse(
+          SlotWebhookResponseCode::InternalServerError,
+          $request->getMember()->user_name,
+          $request->getProductID(),
+          $request->getMember() ? $request->getMember()->balanceFloat : 0,
+          $request->getMember() ? $request->getMember()->balanceFloat : 0,
+          1,
+        );
+      }
+      $validator = $request->check();
 
-       $validator = $request->check();
+      if ($validator->fails()) {
+        // Release Redis lock and return validation error response
+        // tem redis
+        // Redis::del("wallet:lock:$userId");
+        // temp redis
+        $data[] = $validator->getResponse();
+        return response()->json([
+          'data' => $data
+        ]);
+      }
+      $before_balance = $request->getMember() ? $request->getMember()->balanceFloat : 0;
 
-        if ($validator->fails()) {
-          // Release Redis lock and return validation error response
-          // tem redis
-          // Redis::del("wallet:lock:$userId");
-          // temp redis
-          $data[] = $validator->getResponse();
-          return response()->json([
-            'data' => $data
-          ]);
-        }
       DB::beginTransaction();
       try {
         // Create and store the event in the database
-       
+
         $event = $this->createEvent($request);
         // Insert bets using chunking for better performance
         // $seamless_transactions = $this->createWagerTransactions($validator->getRequestTransactions(), $event);
         $seamless_transactions = $this->updateAndCreateWager($batch, $event);
+        $request->getMember()->wallet->refreshBalance();
+        $after_balance = $request->getMember()->balanceFloat;
+        DB::commit();  // Commit only the bet insertion
+        $data[] = SlotWebhookService::buildGscResponse(
+          SlotWebhookResponseCode::Success,
+          $request->getMember()->user_name,
+          $request->getProductID(),
+          $after_balance,
+          $before_balance,
+          $currencyRate->value
+        );
       } catch (\Exception $e) {
         DB::rollBack();
         Redis::del("wallet:lock:$userId");
@@ -53,5 +79,8 @@ class PushBetDataController extends Controller
       }
 
     }
+    return response()->json([
+      'data' => $data
+    ]);
   }
 }
