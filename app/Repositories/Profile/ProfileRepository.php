0f42e755eccd051c10dac5ca46df867401e2c85a
kaungsattkhant <ookaung42@gmail.com>
2025-09-27 20:08:19 +0630

    done

--- app/Repositories/Profile/ProfileRepository.php
+++ app/Repositories/Profile/ProfileRepository.php
@@ -4,19 +4,20 @@ namespace App\Repositories\Profile;
 
 use App\Models\Contact;
 use App\Models\Customer;
+use App\Http\Action\SMSPoh;
 use Illuminate\Support\Facades\DB;
 
 class ProfileRepository implements ProfileInterface
 {
     public function getProfile($request)
-    { 
+    {
         $customer = Customer::select(['id', 'name', 'phone_number', 'verified_at'])
-        ->with(['main_wallet'])
-        ->findOrFail(UserData()->id);
-        $contact=Contact::orderBy('id','asc')->get();
-        $customer->main_money_balance=$customer->main_wallet->balance;
-        $customer->game_money_balance=$customer->balanceFloat;
-        $customer->contact=$contact;
+            ->with(['main_wallet'])
+            ->findOrFail(UserData()->id);
+        $contact = Contact::orderBy('id', 'asc')->get();
+        $customer->main_money_balance = $customer->main_wallet->balance;
+        $customer->game_money_balance = $customer->balanceFloat;
+        $customer->contact = $contact;
         unset($customer['wallet']);
         unset($customer['main_wallet']);
         return $customer;
@@ -26,20 +27,33 @@ class ProfileRepository implements ProfileInterface
         try {
             DB::beginTransaction();
             $customer = Customer::find(UserData()->id);
-            if (!$customer) {
-                ResponseMessage('Customer Not found', 404);
+
+            if (isset($request->otp)) {
+                if (!$customer) {
+                    ResponseMessage('Customer Not found', 404);
+                }
+                if ($customer->getOtpCode() !== $request->otp) {
+                    ResponseMessage('OTP is incorrect, check SMS message again', 400);
+                }
+                $customer->fill([
+                    'password' => $request->new_password,
+                ])->save();
+                // $data["user"] = $customer;
+                // $data["token"] = $customer->createToken('customer_token')->plainTextToken;
+                // $data["code"] = 200;
+                // $data["success"] = true;
+                // $data["message"] = 'Authenticated';
+                // ResponseData($data, 200, true, 'Password updated successfully');
+                DB::commit();
+                ResponseMessage('Password updated successfully', 200);
             }
-            $customer->fill([
-                'password' => $request->new_password,
-            ])->save();
-            // $data["user"] = $customer;
-            // $data["token"] = $customer->createToken('customer_token')->plainTextToken;
-            // $data["code"] = 200;
-            // $data["success"] = true;
-            // $data["message"] = 'Authenticated';
-            // ResponseData($data, 200, true, 'Password updated successfully');
-            DB::commit();
-            ResponseMessage('Password updated successfully',200);
+            $isSuccess = (new SMSPoh($customer))->sendVerifcationCode();
+            if ($isSuccess) {
+                DB::commit();
+                return response()->json(['success' => true, 'message' => 'OTP sent successfully.']);
+            }
+
+
 
         } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()], 500);
