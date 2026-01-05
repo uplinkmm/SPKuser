89809583872fa54208a86c750610fd6cb12ebaa7
kaungsattkhant <ookaung42@gmail.com>
2026-01-05 15:46:20 +0630

    done

--- config/game.php
+++ config/game.php
@@ -6,5 +6,11 @@ return [
         'password' => env('SEAMLESS_PASSWORD'),
         'secret_key' => env('SEAMLESS_SECRET_KEY'),
         'url' => env('SEAMLESS_API_URL'),
+        'SITE_NAME' => env('SITE_NAME'),
+        'SITE_PREFIX' => env('SITE_PREFIX'),
+        'SITE_URL' => env('SITE_URL'),
+        'BUFFALO_PROVIDER_ID' => env('BUFFALO_PROVIDER_ID'),
+        'BUFFALO_GAME_ID' => env('BUFFALO_GAME_ID'),
+        'BUFFALO_GAME_URL' => env('BUFFALO_GAME_URL')
     ],
 ];
