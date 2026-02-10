d1a3a5b0ac9a6e273c63c6c06c669b4b7f2a2f95
NayPaingSoe <naypaingsoe042@gmail.com>
2026-02-10 22:07:51 +0630

    fix: ui

--- resources/js/Components/Auth/ForgotPassword.vue
+++ resources/js/Components/Auth/ForgotPassword.vue
@@ -1,117 +1,157 @@
 <template>
     <main
-        class="w-full h-full min-h-[60vh] mx-auto px-8 pb-2 flex flex-row justify-center items-center relative"
+        class="w-full h-full min-h-screen px-0 flex justify-center flex-row relative bg-black"
     >
-        <div class="">
-            <div class="relative mb-8">
-                <div
-                    @click="changeForgotPassword(false)"
-                    class="absolute left-0 flex items-center justify-center w-8 h-8 rounded-full border border-white cursor-pointer mb-3"
-                >
-                    <i class="fas fa-chevron-left primary-text"></i>
+        <div class="w-full">
+            <div
+                class="flex items-center justify-between text-center pt-8 pb-6 px-20"
+            >
+                <img
+                    src="../../../../public/img/SPK Logo.png"
+                    class="bg-slate-800 p-4 rounded-full w-25 h-25 mb-4"
+                    alt=""
+                />
+                <h3 class="mb-2 text-white text-4xl font-semibold">
+                    မင်္ဂလာပါ
+                </h3>
+            </div>
+
+            <div class="bg-[#FFC529] rounded-3xl px-6 py-6">
+                <div class="relative mb-6">
+                    <button
+                        type="button"
+                        @click="changeForgotPassword(false)"
+                        class="absolute left-0 top-1/2 -translate-y-1/2 text-gray-700"
+                    >
+                        <i class="fas fa-chevron-left text-xl"></i>
+                    </button>
+                    <p
+                        class="text-center text-black text-xl font-semibold mb-0"
+                    >
+                        Forgot Password?
+                    </p>
                 </div>
 
-                <p class="text-center font-semibold primary-text">
-                    Forgot Password?
-                </p>
-            </div>
+                <div class="mb-5">
+                    <p class="text-black font-semibold mb-2">
+                        {{ $t("Phone Number") }}
+                    </p>
+                    <div class="relative">
+                        <i
+                            class="fas fa-phone-alt text-gray-500 absolute left-4 top-1/2 -translate-y-1/2"
+                        ></i>
+                        <input
+                            type="text"
+                            id="phone_number"
+                            v-model="phone_number"
+                            :placeholder="$t('Phone Number')"
+                            pattern="\\d*"
+                            @input="
+                                phone_number = $event.target.value.replace(
+                                    /[^0-9]/g,
+                                    '',
+                                )
+                            "
+                            class="w-full bg-[#D9D9D9] pl-12 pr-28 py-4 rounded-xl text-base text-black placeholder-gray-500 border border-black/30 focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+                        />
+                        <button
+                            type="button"
+                            class="absolute right-3 top-1/2 -translate-y-1/2 text-sm px-3 py-2 rounded-lg bg-black text-white disabled:bg-gray-600"
+                            @click="getOtp"
+                            :disabled="countdown != 0"
+                        >
+                            Get OTP
+                        </button>
+                    </div>
+                </div>
 
-            <div class="mb-4">
-                <label
-                    class="flex w-full py-2 px-2 border border-gray-400 text-sm rounded-md bg-white"
-                >
-                    <input
-                        type="text"
-                        id="phone_number"
-                        v-model="phone_number"
-                        :placeholder="$t('Phone Number')"
-                        pattern="\\d*"
-                        @input="
-                            phone_number = $event.target.value.replace(
-                                /[^0-9]/g,
-                                ''
-                            )
-                        "
-                        class="focus:ring-0 focus:shadow-none focus:outline-none w-full"
-                    />
-                    <button
-                        class="text-xs border-l border-gray-400 pl-1 py-1 flex-shrink-0"
-                        @click="getOtp"
-                        :disabled="countdown != 0"
-                    >
-                        Get OTP
-                    </button>
-                </label>
-            </div>
-            <div class="mb-4">
-                <div class="relative">
-                    <input
-                        type="text"
-                        id="otp"
-                        v-model="otp"
-                        placeholder="OTP"
-                        :disabled="!otpRequested"
-                        pattern="\\d*"
-                        @input="
-                            otp = $event.target.value.replace(/[^0-9]/g, '')
-                        "
-                        class="block w-full py-2 px-2 pr-16 border border-gray-400 text-sm rounded-md bg-white focus:ring-0 focus:shadow-none focus:outline-black"
-                    />
-                    <button
-                        class="absolute right-2 top-1/2 transform -translate-y-1/2 text-xs border-l border-gray-400 pl-8 py-1"
-                    >
-                        <p>
+                <div class="mb-5">
+                    <p class="text-black font-semibold mb-2">OTP</p>
+                    <div class="relative">
+                        <i
+                            class="fas fa-key text-gray-500 absolute left-4 top-1/2 -translate-y-1/2"
+                        ></i>
+                        <input
+                            type="text"
+                            id="otp"
+                            v-model="otp"
+                            placeholder="OTP"
+                            :disabled="!otpRequested"
+                            pattern="\\d*"
+                            @input="
+                                otp = $event.target.value.replace(/[^0-9]/g, '')
+                            "
+                            class="w-full bg-[#D9D9D9] pl-12 pr-24 py-4 rounded-xl text-base text-black placeholder-gray-500 border border-black/30 focus:outline-none focus:ring-1 focus:ring-black focus:border-black disabled:bg-gray-300"
+                        />
+                        <div
+                            class="absolute right-3 top-1/2 -translate-y-1/2 text-sm font-semibold text-gray-700"
+                        >
                             {{
                                 countdown > 0
                                     ? countdown.toString().padStart(2, "0") +
                                       " s"
                                     : "00 s"
                             }}
-                        </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-5">
+                    <p class="text-black font-semibold mb-2">Password</p>
+                    <div class="relative">
+                        <i
+                            class="fas fa-lock text-gray-500 absolute left-4 top-1/2 -translate-y-1/2"
+                        ></i>
+                        <input
+                            :type="show_password ? 'text' : 'password'"
+                            id="password"
+                            v-model="password"
+                            :disabled="!otpRequested"
+                            placeholder="Password"
+                            class="w-full bg-[#D9D9D9] pl-12 pr-12 py-4 rounded-xl text-base text-black placeholder-gray-500 border border-black/30 focus:outline-none focus:ring-1 focus:ring-black focus:border-black disabled:bg-gray-300"
+                        />
+                        <button
+                            type="button"
+                            @click="show_password = !show_password"
+                            class="absolute right-4 top-1/2 -translate-y-1/2 text-gray-600"
+                        >
+                            <i
+                                v-if="!show_password"
+                                class="far fa-eye text-lg"
+                            ></i>
+                            <i v-else class="far fa-eye-slash text-lg"></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div class="mb-10">
+                    <p class="text-black font-semibold mb-2">
+                        Confirm Password
+                    </p>
+                    <div class="relative">
+                        <i
+                            class="fas fa-lock text-gray-500 absolute left-4 top-1/2 -translate-y-1/2"
+                        ></i>
+                        <input
+                            :type="show_password ? 'text' : 'password'"
+                            id="password_confirmation"
+                            v-model="password_confirmation"
+                            placeholder="Confirm Password"
+                            :disabled="!otpRequested"
+                            class="w-full bg-[#D9D9D9] pl-12 pr-4 py-4 rounded-xl text-base text-black placeholder-gray-500 border border-black/30 focus:outline-none focus:ring-1 focus:ring-black focus:border-black disabled:bg-gray-300"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <button
+                        @click="forgotPassword"
+                        :disabled="!otpRequested"
+                        class="block disabled:bg-gray-600 w-full py-4 px-2 text-base rounded-xl bg-[#5271FF] text-white font-semibold focus:ring-0 focus:shadow-none focus:outline-none"
+                    >
+                        Confirm
                     </button>
                 </div>
-            </div>
-
-            <div class="mb-4 relative">
-                <input
-                    :type="show_password ? 'text' : 'password'"
-                    id="password"
-                    v-model="password"
-                    :disabled="!otpRequested"
-                    placeholder="Password"
-                    class="block w-full py-2 px-2 pr-10 border border-gray-400 text-sm rounded-md bg-white focus:ring-0 focus:shadow-none focus:outline-none"
-                />
-                <i
-                    v-if="!show_password"
-                    @click="show_password = !show_password"
-                    class="far fa-eye text-lg absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                ></i>
-                <i
-                    v-if="show_password"
-                    @click="show_password = !show_password"
-                    class="far fa-eye-slash text-lg absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer"
-                ></i>
-            </div>
-
-            <div class="mb-8">
-                <input
-                    :type="show_password ? 'text' : 'password'"
-                    id="password_confirmation"
-                    v-model="password_confirmation"
-                    placeholder="Confirm Password"
-                    :disabled="!otpRequested"
-                    class="block w-full py-2 px-2 border border-gray-400 text-sm rounded-md bg-white focus:ring-0 focus:shadow-none focus:outline-none"
-                />
-            </div>
-
-            <div>
-                <button
-                    @click="forgotPassword"
-                    :disabled="!otpRequested"
-                    class="block w-full py-2.5 px-2 text-sm rounded-md bg-[#000] border border-[#E4BD1B] text-white focus:ring-0 focus:shadow-none focus:outline-none"
-                >
-                    Confirm
-                </button>
             </div>
         </div>
         <form method="POST" id="signin-form" ref="signinForm" action="/login">
@@ -208,7 +248,7 @@ export default {
             ) {
                 this.setErrorBox(
                     true,
-                    "Password must be at least 6 characters long."
+                    "Password must be at least 6 characters long.",
                 );
 
                 return;
@@ -220,7 +260,7 @@ export default {
             formData.append("password", this.password);
             formData.append(
                 "password_confirmation",
-                this.password_confirmation
+                this.password_confirmation,
             );
             formData.append("otp", this.otp);
             formData.append("fcm_token", this.fcmToken); //from mixin
