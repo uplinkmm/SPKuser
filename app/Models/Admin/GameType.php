18acbf78415579751b6e6656dd91bf5ca89861da
kaungsattkhant <ookaung42@gmail.com>
2026-02-11 20:22:57 +0630

    added seach and payment transaction id

--- app/Models/Admin/GameType.php
+++ app/Models/Admin/GameType.php
@@ -2,8 +2,10 @@
 
 namespace App\Models\Admin;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class GameType extends Model
 {
@@ -28,4 +30,14 @@ class GameType extends Model
     {
         return asset('assets/img/game_type/'.$this->img);
     }
+
+    public function scopeFilter(Builder $query ,array $data) : Builder {
+        return $query
+            ->when(!empty($data['search_input']), function ($q) use ($data) {
+                $q->whereHas('products', function ($p) use ($data) {
+                        $p->where('name', 'like', '%' . $data['search_input'] . '%');
+                    });
+            });
+    }
+
 }
