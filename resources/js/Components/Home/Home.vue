31fddad06b15cb3e7b245758c0bdae322c486533
NayPaingSoe <naypaingsoe042@gmail.com>
2026-02-11 20:48:27 +0630

    fix: ui

--- resources/js/Components/Home/Home.vue
+++ resources/js/Components/Home/Home.vue
@@ -416,20 +416,20 @@
                                 <div class="mb-12">
                                     <div class="w-full">
                                         <div
-                                            class="w-full grid grid-cols-2 gap-x-4 lg:gap-x-6 gap-y-4"
+                                            class="w-full grid grid-cols-3 gap-x-4 gap-y-6"
                                         >
                                             <div class="relative">
                                                 <a
                                                     href="/buffalo_rooms"
-                                                    class="cursor-pointer"
+                                                    class="cursor-pointer block"
                                                 >
                                                     <img
                                                         src="../../../../public/img/buffalo/750x500.png"
-                                                        class="w-full aspect-[3/2] rounded-lg"
+                                                        class="w-full aspect-square rounded-2xl object-cover"
                                                         alt=""
                                                     />
                                                     <p
-                                                        class="text-white text-center pt-1 text-sm"
+                                                        class="text-white text-left pt-2 text-lg leading-tight truncate"
                                                     >
                                                         Buffalo game
                                                     </p>
@@ -458,17 +458,17 @@
                                                             name: selectedGameType.name,
                                                         },
                                                     )}`"
-                                                    class="cursor-pointer"
+                                                    class="cursor-pointer block"
                                                 >
                                                     <img
-                                                        class="w-full aspect-[3/2] rounded-lg"
+                                                        class="w-full aspect-square rounded-2xl object-cover"
                                                         :src="
                                                             product.pivot.image
                                                         "
                                                         alt=""
                                                     />
                                                     <p
-                                                        class="text-white text-center pt-1 text-sm"
+                                                        class="text-white text-left pt-2 text-lg leading-tight truncate"
                                                     >
                                                         {{ product.name }}
                                                     </p>
