d68d691ed56d8fcbc618ac62d2d9f60d62ac82b1
NayPaingSoe <naypaingsoe042@gmail.com>
2026-02-19 13:28:08 +0630

    fix: ui

--- resources/js/Components/Betting/2Dbetting.vue
+++ resources/js/Components/Betting/2Dbetting.vue
@@ -2195,13 +2195,15 @@ export default {
             this.quickBetting(Array.from(digitsSet));
         },
         numbersEndingWith(num) {
-            const digits = [];
+            const digitsSet = new Set();
             for (let i = 0; i < 100; i++) {
-                if (String(i).endsWith(num)) {
-                    digits.push(String(i).padStart(2, "0"));
+                const padded = String(i).padStart(2, "0");
+                if (padded.endsWith(String(num))) {
+                    digitsSet.add(padded);
+                    digitsSet.add(padded.split("").reverse().join(""));
                 }
             }
-            this.quickBetting(digits);
+            this.quickBetting(Array.from(digitsSet));
         },
         breakNumbers(sum1, sum2) {
             const digits = [];
